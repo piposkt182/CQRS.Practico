489b8ad2b7058a54f60406ec18763d65605decf4
Andres Marquez <piposkt182@gmail.com>
2025-05-30 14:51:24 -0500

    merge wiht AI

--- MyApp.Application/obj/Debug/net8.0/MyApp.Application.AssemblyInfo.cs
+++ MyApp.Application/obj/Debug/net8.0/MyApp.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ededcbbe7382d64ebb4f7efa3592ac3033e11714")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
